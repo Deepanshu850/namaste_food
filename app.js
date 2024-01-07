24c5f262b83e4c5f9c0aaf5dd031ef125e33b2a5
Deepanshu850 <deepuanshu123as@gmail.com>
2024-01-07 19:01:31 +0530

    writing html using react object not using jsx

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// const heading = React.createElement("h1", {}, "hello world from react");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+
+// ReactElement(Object)=>HTML(Browser Understant)
+
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "i ma h1 tag"),
+        React.createElement("h2", {}, "i ma h2 tag")
+        ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+// this will make our life hectic
+// jsx will we introduced 
+
