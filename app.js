fed1b57b28c29e19248c773ddc8f4a3bcdc4a2d1
Deepanshu850 <deepuanshu123as@gmail.com>
2024-01-09 12:23:09 +0530

    more about createElement

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {}, "  world   react");
+// const heading = React.createElement("h1", {}, "  world   react");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // order of file matter
 
@@ -14,8 +14,17 @@ const heading = React.createElement("h1", {}, "  world   react");
 //         React.createElement("h2", {}, "i ma h2 tag")
 //         ])
 // );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 // this will make our life hectic
 // jsx will we introduced 
 
+
+const heading = React.createElement("h1", { id: "heading" }, "Namaste Reactey"); //createElement is obect when we render then it will become html element
+
+// jsx
+const jsxHeading = <h1>Namaste Using React</h1> //jsx is not html but jsx is html syntax
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading); //take object and converted into html 
