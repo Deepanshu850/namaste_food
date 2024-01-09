c847fb6a8e82a8d20fd96c9d31b8998d33cca263
Deepanshu850 <deepuanshu123as@gmail.com>
2024-01-09 13:38:56 +0530

    commit

--- app.js
+++ app.js
@@ -20,11 +20,24 @@ import ReactDOM from "react-dom/client";
 // jsx will we introduced 
 
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste Reactey"); //createElement is obect when we render then it will become html element
+const heading = React.createElement("h1", { id: "heading" }, "Namaste Reacte"); //createElement is obect when we render then it will become html element
 
-// jsx
-const jsxHeading = <h1>Namaste Using React</h1> //jsx is not html but jsx is html syntax
+// jsx ( transpile before  it reaches the js engine) parcel is doing this parcel gives this job to babel and internally babel is doing 
+const jsxHeading = (<h1 className="heading" >Namaste Using React</h1>);//jsx is not html but jsx is html syntax
 
+
+
+// React Component
+// Class based component
+// functional bases componenet
+
+const HeadingComponent = () => {
+    return (
+        <h1> Namaste react function Componenets</h1>
+    )
+}
+
+//jsx -> reactcreateElement{babel is doing this } ->js element  
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading); //take object and converted into html 
+root.render(jsxHeading); //take object and converted into html 
