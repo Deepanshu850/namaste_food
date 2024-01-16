b769808ebbfb619243b0002041e12ad5019c7536
Deepanshu850 <deepuanshu123as@gmail.com>
2024-01-16 16:08:23 +0530

    fetching users data from github api using class based components architechture

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,27 +6,33 @@ class UserClass extends React.Component {
         super(props);
 
         this.state = {
-            count: 0,
+            userInfo: {
+                name: "Dummy-name",
+                location: "Default"
+            },
         }
     }
-    componentDidMount() {
+    async componentDidMount() {
         // use for api call in class basesd  Components
         // just like we use useeffect in functional based componenets
+        const data = await fetch("https://api.github.com/users/Deepanshu850");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo: json,
+        });
+
+
     };
 
     render() {
-        const { name, location } = this.props;
+
         return (<div className="user-card-class">
-            <h1>Count:{this.state.count}</h1>
-            <button onClick={() => {
-                // never update state direct state varible directly
-                this.setState({
-                    count: this.state.count + 1,
-                });
-            }}>Button</button>
+            <h1> {this.state.userInfo.name}</h1>
+            <h2> {this.state.userInfo.location}</h2>
+
             {/* <h1>Count:{this.state.count2}</h1> */}
-            <h2> Name:  {name}</h2>
-            <h3>Location: {location}</h3>
+
             <h3>contact : 95998408734</h3>
             <h3> Btech : CSE</h3>
         </div>);
