fb5be0c6503c04b5cb23cd961ea5b5bcf5d561b6
Deepanshu850 <deepuanshu123as@gmail.com>
2024-01-16 14:06:24 +0530

    understanding props and state in classbases componenets

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,11 +4,18 @@ class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0,
+            count2: 3,
+        }
     }
 
     render() {
         const { name, location } = this.props;
         return (<div className="user-card-class">
+            <h1>Count:{this.state.count}</h1>
+            <h1>Count:{this.state.count2}</h1>
             <h2> Name:  {name}</h2>
             <h3>Location: {location}</h3>
             <h3>contact : 95998408734</h3>
